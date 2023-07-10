849029bdf4ca10ba0c914892e239d25a501f183c
Anthony Guerrero <anthony@anthony.moe>
2023-07-10 10:22:11 -0700

    external rc file

--- build.rs
+++ build.rs
@@ -2,6 +2,14 @@ extern crate winres;
 
 fn main() {
     let mut res = winres::WindowsResource::new();
+    res.set_resource_file("resources.rc");
+    res.compile().unwrap();
+}
+
+/*
+fn main() {
+    let mut res = winres::WindowsResource::new();
     res.set_icon("snapin.ico");
     res.compile().unwrap();
 }
+*/
